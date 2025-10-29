68635894fdcce26fe82d6876ccf00e991eaefb99
rux <redoxrux@gmail.com>
2025-10-29 05:44:16 +0000

    Create 01_gradient_descent_basics.py with module docstring

--- 01_gradient_descent_basics.py
+++ 01_gradient_descent_basics.py
@@ -1,1 +1,10 @@
+"""
+Gradient Descent Basics
 
+Finding the minimum of f(x) = (x - 3)² using gradient descent.
+
+Questions to answer:
+1. What is the derivative of f(x) = (x - 3)²?
+2. How do you use that derivative to know which direction is "downhill"?
+3. How do you update x iteratively to reach the minimum?
+"""
